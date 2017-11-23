84b879a564cbaf5f1478a38dc37c259395420b98
cixiu <cheng597604817@qq.com>
2017-11-24 01:07:41 +0800

    finish m-header component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
+import MHeader from 'components/m-header/m-header';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        Hello World
+        <MHeader />
       </div>
     );
   }
